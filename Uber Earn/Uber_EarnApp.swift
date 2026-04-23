0a68844342c73d23b562859d4033bcb163288020
erratinsilentio <38041536+erratinsilentio@users.noreply.github.com>
2026-04-23 04:15:20 +0200

    Initial Commit

---
+++ Uber Earn/Uber_EarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_EarnApp.swift
+//  Uber Earn
+//
+//  Created by Kacper Kondracki on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Uber_EarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
